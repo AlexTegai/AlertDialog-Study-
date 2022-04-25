2ce08f2387c41ca532d706dba2fe03b7841ec14f
Aleksandr Tegai <shura.tegay@mail.ru>
2022-04-25 16:45:52 +0300

    Start AlertDialogTest

---
+++ app/src/main/java/com/example/alertdialog/DialogsLevel1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.alertdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DialogsLevel1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialogs_level1)
+    }
+}
